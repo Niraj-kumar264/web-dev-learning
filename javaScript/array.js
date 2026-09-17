a42af67e69c14261de7fdaa530d7110f2575e9dc
Niraj kumar <roydhiraj.7350@gmail.com>
2026-09-17 16:43:09 +0530

    array and factorial using javaScript

---
+++ javaScript/array.js
@@ -0,0 +1,15 @@
+// console.log("this is a demonstration of array in javaScript ");
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr[4]);
+// arr[0]= 56;
+// console.log(arr);
+
+//shortcut to create a array
+let a = 8;/////shows the element in the array
+function fact(number) {
+    let arr = Array.from(Array(number).keys())
+    console.log(arr)
+}
+fact(a)
